eeddf1344455362125696ba4fa2a0cb558f0d9e2
shadongo <shadongo@vassar.edu>
2014-01-27 17:45:21 -0500

    First snapshot

---
+++ hello_world/src/hello_world/Hello_world.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_world;
+
+/**
+ *
+ * @author shadongo
+ */
+public class Hello_world {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
